14142979dec31dcb053487f89f8a1059f9fbd7f7
benjesty2002 <benjesty2002@gmail.com>
2023-07-23 20:32:41 +0100

    add views

---
+++ build_statements/driver_standings_leads.sql
@@ -0,0 +1,27 @@
+CREATE VIEW IF NOT EXISTS driver_standings_leads AS
+SELECT  r.year,
+        r.round as round_num,
+        r.name as race_name,
+        d1.forename as forename_1,
+        d1.surname as surname_1,
+        s1.points as points_1,
+        d2.forename as forename_2,
+        d2.surname as surname_2,
+        s2.points as points_2,
+        ROUND(s1.points / s2.points, 2) as points_ratio,
+        s1.points - s2.points as points_lead,
+        r.raceId,
+        d1.driverId as driver_1_id,
+        d2.driverId as driver_2_id
+FROM driver_standings s1
+JOIN driver_standings s2 
+    ON s1.raceId = s2.raceId
+    AND s1.position = 1
+    AND s2.position = 2
+JOIN races r
+    ON s1.raceId = r.raceId
+JOIN drivers d1
+    ON s1.driverId = d1.driverId
+JOIN drivers d2
+    ON s2.driverId = d2.driverId
+ORDER BY points_ratio DESC, r.year ASC, r.round ASC
---
+++ build_statements/drivers_champions.sql
@@ -0,0 +1,21 @@
+CREATE VIEW final_standings AS
+WITH ranked_races AS (
+    SELECT *, RANK() OVER (PARTITION BY year ORDER BY round DESC) as rn
+    FROM races
+)
+, final_races AS (
+    SELECT *
+    FROM ranked_races
+    WHERE rn = 1
+)
+SELECT  fr.year,
+        s.position,
+        s.driverId,
+        s.points
+FROM driver_standings s
+JOIN final_races fr
+    ON s.raceId = fr.raceId
+    AND fr.rn = 1
+-- WHERE s.position = 1
+ORDER BY fr.year, s.position
+;
